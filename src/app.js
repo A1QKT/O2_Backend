17b54935472b3ecdc505e872c16f1b6a380ac080
A1QKT <taquangkhoi1232002@gmail.com>
2022-04-29 21:46:26 +0700

    change port

--- src/app.js
+++ src/app.js
@@ -7,6 +7,9 @@ const app = express();
 app.use(express.static("public"))
 app.use(express.json());
 
+app.get("/", (req, res) => {
+    res.send("hello O2");
+})
 app.use(require("./modules/authentication/authentication"));
 
 module.exports = app;
